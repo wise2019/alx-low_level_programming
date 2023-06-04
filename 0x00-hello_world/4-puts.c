6a8afb3e06f8f51d0a028a97103308052c3da9e0
wise2019 <wisearthur1@gmail.com>
2023-06-03 22:18:15 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is a like a multilingual
+ * puzzle, followed by new lineline."
+ * return 0.
+ * /
+ 
+
+ int main(void)
+ {
+ 
+ 	puts("\"programming is like a multilingual puzzle");
+	
+	retrun (0);
+
+ 
+ }
