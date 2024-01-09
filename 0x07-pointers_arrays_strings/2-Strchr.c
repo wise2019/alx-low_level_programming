743da89618955f50be3e789f1c0d52becd60551d
wise2019 <wisearthur1@gmail.com>
2024-01-09 11:00:55 -0800

    fix

--- 0x07-pointers_arrays_strings/2-Strchr.c
+++ 0x07-pointers_arrays_strings/2-Strchr.c
@@ -1,19 +1,24 @@
 #include "main.h"
+
 /**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
- */
+  * _strchr - main function
+  *
+  * @s: Function parameter
+  *
+  * @c: Function parameter
+  *
+  * Return: Always o.
+  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int k;
 
-	for (; s[i] >= '\0'; i++)
+	for (k = 0; s[k] >= '\0'; k++)
 	{
-		if (s[i] == c)
-			return (&s[i]);
+	if (s[k] == c)
+	return (s + k);
 	}
 	return (0);
 }
+
 
