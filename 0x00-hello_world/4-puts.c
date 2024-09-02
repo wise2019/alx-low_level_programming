b6024dd8424870fbc7890e1971f806c0ea0a375a
wise2019 <wisearthur1@gmail.com>
2024-09-02 06:06:13 -0700

    up

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/** 
- * Main :- Entry point 
+/**
+ * Main :- Entry point
  *
- * Return: Always 0 (Sucess)
+ * Return: Always 0 (Sucess
  */
- 
+
 int main (void)
 {
 puts("\"Programming is like building a multilingual puzzle");
