da360140aee0b0291b13f72bd24407f7b97d6c3d
wise2019 <wisearthur1@gmail.com>
2024-09-17 04:07:12 -0700

    up

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h
+
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	for (i = 0; i <26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
